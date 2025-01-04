33f9086e7f1bab99030da73c8da5950b1db62dae
murad <muradkv@gmail.com>
2025-01-04 03:47:29 +0300

    Initial commit

---
+++ UIKit section/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKit section
+//
+//  Created by murad on 04.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .brown
+    }
+
+
+}
+
